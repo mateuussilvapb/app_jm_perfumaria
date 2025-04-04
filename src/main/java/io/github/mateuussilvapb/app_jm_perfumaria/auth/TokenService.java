2a201d084e16e1b31e26a1262ce3eb02eafba944
Mateus Dias <mateuussilvapb@gmail.com>
2025-04-04 19:13:39 -0300

    [feature] [sp01] [mssousa] - Configuração de Endpoint de recuperação de token pela aplicação.

---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/auth/TokenService.java
@@ -0,0 +1,38 @@
+package io.github.mateuussilvapb.app_jm_perfumaria.auth;
+
+import io.github.mateuussilvapb.app_jm_perfumaria.auth.dtos.TokenResponse;
+import io.github.mateuussilvapb.app_jm_perfumaria.auth.dtos.UserDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class TokenService {
+
+    private final WebClient webClient;
+
+    public TokenService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public TokenResponse getToken(UserDTO user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        var formData = BodyInserters
+                .fromFormData("username", user.username())
+                .with("password", user.password())
+                .with("client_id", user.clientID())
+                .with("grant_type", user.grantType());
+
+        return webClient.post()
+                .uri("http://localhost:8080/realms/manager/protocol/openid-connect/token")
+                .headers(httpHeaders -> httpHeaders.addAll(headers))
+                .body(formData)
+                .retrieve()
+                .bodyToMono(TokenResponse.class)
+                .block();
+    }
+}
---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/auth/dtos/TokenResponse.java
@@ -0,0 +1,33 @@
+package io.github.mateuussilvapb.app_jm_perfumaria.auth.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("refresh_expires_in")
+    private int refreshExpiresIn;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("session_state")
+    private String sessionState;
+
+    private String scope;
+}
