022b5f9dae3579272917f5721947c6eb5e867874
Mateus Dias <mateuussilvapb@gmail.com>
2025-04-04 18:06:13 -0300

    [feature] [sp01] [mssousa] - projeto base

---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/AppJmPerfumariaApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.app_jm_perfumaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJmPerfumariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJmPerfumariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateuussilvapb/app_jm_perfumaria/AppJmPerfumariaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateuussilvapb.app_jm_perfumaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJmPerfumariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
