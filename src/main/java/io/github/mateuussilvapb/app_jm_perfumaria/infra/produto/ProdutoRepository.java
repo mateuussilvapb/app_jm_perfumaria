6c52606dab6cb242d03009930e43a28b74716d0c
Mateus Dias <mateuussilvapb@gmail.com>
2025-04-25 08:11:30 -0300

    [feature] [sp01] [mssousa] - Ajustes no Repositório de produto, criação do service de Produto e criação do Specification de Produto para consultar Produtos por filtro.

--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/produto/ProdutoRepository.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/produto/ProdutoRepository.java
@@ -1,8 +1,35 @@
 package io.github.mateuussilvapb.app_jm_perfumaria.infra.produto;
 
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.dto.ProdutoAutocompleteDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Situacao;
+import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long>, JpaSpecificationExecutor<Produto> {
+
+    @Query("SELECT p FROM Produto p WHERE p.status = :status")
+    List<Produto> findAllProdutosByStatus(@Param("status") Status status);
+
+    @Query("""
+                        SELECT new io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.dto.ProdutoAutocompleteDTO(p.id, p.nome)
+                        FROM Produto p
+                        WHERE p.status = :status
+                          AND (
+                               :nome IS NULL
+                               OR :nome = ''
+                               OR LOWER(p.nome) LIKE LOWER(CONCAT('%', :nome, '%'))
+                               OR LOWER(p.descricao) LIKE LOWER(CONCAT('%', :nome, '%'))
+                          )
+            """)
+    List<ProdutoAutocompleteDTO> findAllProdutosByStatusAutocompleteDTO(@Param("nome") String nome, @Param("status") Status status);
+
+    @Query("SELECT p FROM Produto p WHERE p.situacao = :situacao AND p.status = :status")
+    List<Produto> findAllProdutosBySituacaoAndStatus(@Param("situacao") Situacao situacao, @Param("status") Status status);
 }
---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/produto/ProdutoService.java
@@ -0,0 +1,100 @@
+package io.github.mateuussilvapb.app_jm_perfumaria.infra.produto;
+
+import io.github.mateuussilvapb.app_jm_perfumaria.config.persistence.SequenceService;
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.dto.CreateUpdateProdutoDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.dto.ProdutoAutocompleteDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.dto.ProdutoFiltersDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.exceptions.ProdutoNotFoundException;
+import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.interfaces.ProdutoToProdutoDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Situacao;
+import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Status;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final SequenceService sequenceService;
+    private final ProdutoToProdutoDTO atualizarProdutoMapper;
+
+    @Transactional
+    public List<Produto> findAllProdutosAtivos() {
+        return this.produtoRepository.findAllProdutosByStatus(Status.ATIVO);
+    }
+
+    @Transactional
+    public List<Produto> findAllProdutosInativos() {
+        return this.produtoRepository.findAllProdutosByStatus(Status.INATIVO);
+    }
+
+    @Transactional
+    public List<Produto> findAllProdutosAtivosByTermAndStatus(String searchTerm, Status status) {
+        List<Produto> produtos;
+
+        if (status == Status.ATIVO) {
+            produtos = this.findAllProdutosAtivos();
+        } else {
+            produtos = this.findAllProdutosInativos();
+        }
+
+        if (StringUtils.isNotBlank(searchTerm)) {
+            produtos = produtos.stream()
+                    .filter(produto -> produto.matchSearchTerm(searchTerm))
+                    .collect(Collectors.toList());
+        }
+
+        return produtos;
+    }
+
+    @Transactional
+    public List<ProdutoAutocompleteDTO> findAllProdutoAutocompleteByTermAndStatus(String searchTerm, Status status) {
+        return this.produtoRepository.findAllProdutosByStatusAutocompleteDTO(searchTerm, status);
+    }
+
+    @Transactional
+    public List<Produto> findAllProdutoBySituacaoAndStatus(Situacao situacao, Status status) {
+        return this.produtoRepository.findAllProdutosBySituacaoAndStatus(situacao, status);
+    }
+
+    @Transactional
+    public Produto findProdutoById(Long id) {
+        return this.produtoRepository.findById(id).orElseThrow(() -> new ProdutoNotFoundException(id));
+    }
+
+    @Transactional
+    public Page<Produto> findProdutoByFilters(ProdutoFiltersDTO filtros, Pageable pageable) {
+        return produtoRepository
+                .findAll(ProdutoSpecification.filtrar(filtros), pageable);
+    }
+
+    @Transactional
+    public void deleteProdutoById(Long id) {
+        var produto = this.findProdutoById(id);
+        produto.setStatus(Status.INATIVO);
+        produto.setSituacao(Situacao.CADASTRO_FINALIZADO);
+        this.updateProduto(id, atualizarProdutoMapper.toDto(produto));
+    }
+
+    @Transactional
+    public Produto updateProduto(Long id, CreateUpdateProdutoDTO updatedProduto) {
+        var produto = this.findProdutoById(id);
+        //TODO: Necessário implementar service de Categoria e Marca para validação de ids
+        return null;
+    }
+
+    @Transactional
+    public Produto createProduto(CreateUpdateProdutoDTO createdProduto) {
+        //TODO: Necessário implementar service de Categoria e Marca para validação de ids
+        return null;
+    }
+
+}
