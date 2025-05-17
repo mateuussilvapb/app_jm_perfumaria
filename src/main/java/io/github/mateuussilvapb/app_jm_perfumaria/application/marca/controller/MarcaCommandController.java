16d4cf93ec1d04a3aa4c6f3d21162b27242e11a3
Mateus Dias <mateuussilvapb@gmail.com>
2025-05-17 10:17:09 -0300

    [feature] [sp01] [mssousa] - Query e Command Controller de Marca

---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/marca/controller/MarcaCommandController.java
@@ -0,0 +1,40 @@
+package io.github.mateuussilvapb.app_jm_perfumaria.application.marca.controller;
+
+import io.github.mateuussilvapb.app_jm_perfumaria.application.common.dto.CreateUpdateDto;
+import io.github.mateuussilvapb.app_jm_perfumaria.application.marca.command.MarcaCommandService;
+import io.github.mateuussilvapb.app_jm_perfumaria.domain.marca.Marca;
+import jakarta.annotation.security.RolesAllowed;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/marcas/command")
+public class MarcaCommandController {
+
+    private final MarcaCommandService marcaCommandService;
+
+    @PostMapping
+    @RolesAllowed({"manager", "admin"})
+    public ResponseEntity<Marca> save(@RequestBody CreateUpdateDto marca) {
+        Marca saved = marcaCommandService.create(marca);
+        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    @RolesAllowed({"admin", "manager"})
+    public ResponseEntity<Marca> update(@RequestBody CreateUpdateDto marcaDTO, @PathVariable String id) {
+        Marca marca = marcaCommandService.update(Long.parseLong(id), marcaDTO);
+        return new ResponseEntity<>(marca, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    @RolesAllowed({"admin", "manager"})
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        marcaCommandService.deleteById(Long.parseLong(id));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/marca/controller/MarcaQueryController.java
@@ -0,0 +1,70 @@
+package io.github.mateuussilvapb.app_jm_perfumaria.application.marca.controller;
+
+import io.github.mateuussilvapb.app_jm_perfumaria.application.common.dto.AutocompleteDTO;
+import io.github.mateuussilvapb.app_jm_perfumaria.application.marca.query.MarcaQueryService;
+import io.github.mateuussilvapb.app_jm_perfumaria.domain.marca.Marca;
+import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Status;
+import jakarta.annotation.security.RolesAllowed;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/marcas/query")
+public class MarcaQueryController {
+
+    private final MarcaQueryService marcaQueryService;
+
+    @GetMapping
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<Marca>> getAll() {
+        List<Marca> marcas = marcaQueryService.findAllAtivos();
+        return new ResponseEntity<>(marcas, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<Marca> getById(@PathVariable String id) {
+        return new ResponseEntity<>(marcaQueryService.findById(Long.parseLong(id)), HttpStatus.OK);
+    }
+
+    @GetMapping("/ativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<Marca>> getAllAtivos() {
+        return new ResponseEntity<>(marcaQueryService.findAllAtivos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/inativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<Marca>> getAllInativos() {
+        return new ResponseEntity<>(marcaQueryService.findAllInativos(), HttpStatus.OK);
+    }
+
+    @GetMapping("/searchByTerm/ativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<Marca>> getAtivosByTerm(@RequestParam(name = "term", required = false) String searchTerm) {
+        return new ResponseEntity<>(marcaQueryService.findAllByTermAndStatus(searchTerm, Status.ATIVO), HttpStatus.OK);
+    }
+
+    @GetMapping("/searchByTerm/inativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<Marca>> getInativosByTerm(@RequestParam(name = "term", required = false) String searchTerm) {
+        return new ResponseEntity<>(marcaQueryService.findAllByTermAndStatus(searchTerm, Status.INATIVO), HttpStatus.OK);
+    }
+
+    @GetMapping("/searchByTerm/autocomplete/ativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<AutocompleteDTO>> getAtivosByTermAutocomplete(@RequestParam(name = "term", required = false) String searchTerm) {
+        return new ResponseEntity<>(marcaQueryService.findAllToAutocompleteByTermAndStatus(searchTerm, Status.ATIVO), HttpStatus.OK);
+    }
+
+    @GetMapping("/searchByTerm/autocomplete/inativos")
+    @RolesAllowed({"admin", "employee", "manager"})
+    public ResponseEntity<List<AutocompleteDTO>> getInativosByTermAutocomplete(@RequestParam(name = "term", required = false) String searchTerm) {
+        return new ResponseEntity<>(marcaQueryService.findAllToAutocompleteByTermAndStatus(searchTerm, Status.INATIVO), HttpStatus.OK);
+    }
+}
