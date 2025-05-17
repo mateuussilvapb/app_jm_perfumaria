d0a4d2009141332e0385d691da71f68b9bd34e7e
Mateus Dias <mateuussilvapb@gmail.com>
2025-05-17 11:49:37 -0300

    [feature] [sp01] [mssousa] - Alterando nome de interface IEntradaEstoqueRepository.java

--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/entradaEstoque/repository/IEntradaEstoqueRepository.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/entradaEstoque/repository/IEntradaEstoqueRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntradaEstoqueRepository extends JpaRepository<EntradaEstoque, Long> {
+public interface IEntradaEstoqueRepository extends JpaRepository<EntradaEstoque, Long> {
 }
