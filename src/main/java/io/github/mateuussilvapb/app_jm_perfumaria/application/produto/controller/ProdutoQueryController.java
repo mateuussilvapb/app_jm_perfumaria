fc8d0b8cca35f3f96590c026d23a1197c7f388ab
Mateus Dias <mateuussilvapb@gmail.com>
2025-05-17 10:59:28 -0300

    [feature] [sp01] [mssousa] - Utilizando Specification para realizar todas as consultas relacionadas a Produto

--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/produto/controller/ProdutoQueryController.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/produto/controller/ProdutoQueryController.java
@@ -4,7 +4,6 @@ import io.github.mateuussilvapb.app_jm_perfumaria.application.common.dto.Autocom
 import io.github.mateuussilvapb.app_jm_perfumaria.application.produto.dto.ProdutoFiltersDTO;
 import io.github.mateuussilvapb.app_jm_perfumaria.application.produto.query.ProdutoQueryService;
 import io.github.mateuussilvapb.app_jm_perfumaria.domain.produto.Produto;
-import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Situacao;
 import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Status;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
@@ -26,49 +25,12 @@ public class ProdutoQueryController {
 
     private final ProdutoQueryService produtoQueryService;
 
-    @GetMapping
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getAll() {
-        return new ResponseEntity<>(produtoQueryService.findAll(), HttpStatus.OK);
-    }
-
     @GetMapping("/{id}")
     @RolesAllowed({"admin", "employee", "manager"})
     public ResponseEntity<Produto> getById(
             @PathVariable String id
     ) {
         return new ResponseEntity<>(produtoQueryService.findById(Long.parseLong(id)),
-                HttpStatus.OK);
-    }
-
-    @GetMapping("/ativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getAllAtivos() {
-        return new ResponseEntity<>(produtoQueryService.findAllAtivos(), HttpStatus.OK);
-    }
-
-    @GetMapping("/inativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getAllInativos() {
-        return new ResponseEntity<>(produtoQueryService.findAllInativos(), HttpStatus.OK);
-    }
-
-    @GetMapping("/searchByTerm/ativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getAtivosByTerm(
-            @RequestParam(name = "term", required = false) String searchTerm
-    ) {
-        return new ResponseEntity<>(produtoQueryService.findAllByTermAndStatus(searchTerm, Status.ATIVO),
-                HttpStatus.OK);
-    }
-
-    @GetMapping("/searchByTerm/inativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getInativosByTerm(
-            @RequestParam(name = "term", required = false) String searchTerm
-    ) {
-        return new ResponseEntity<>(produtoQueryService.findAllByTermAndStatus(searchTerm,
-                Status.INATIVO),
                 HttpStatus.OK);
     }
 
@@ -88,26 +50,6 @@ public class ProdutoQueryController {
             @RequestParam(name = "term", required = false) String searchTerm
     ) {
         return new ResponseEntity<>(produtoQueryService.findAllToAutocompleteByTermAndStatus(searchTerm,
-                Status.INATIVO),
-                HttpStatus.OK);
-    }
-
-    @GetMapping("/searchBySituacao/ativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getAtivosBySituacao(
-            @RequestParam(name = "situacao") Situacao situacao
-    ) {
-        return new ResponseEntity<>(produtoQueryService.findAllBySituacaoAndStatus(situacao,
-                Status.ATIVO),
-                HttpStatus.OK);
-    }
-
-    @GetMapping("/searchBySituacao/inativos")
-    @RolesAllowed({"admin", "employee", "manager"})
-    public ResponseEntity<List<Produto>> getInativosBySituacao(
-            @RequestParam(name = "situacao") Situacao situacao
-    ) {
-        return new ResponseEntity<>(produtoQueryService.findAllBySituacaoAndStatus(situacao,
                 Status.INATIVO),
                 HttpStatus.OK);
     }
--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/produto/query/ProdutoQueryService.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/produto/query/ProdutoQueryService.java
@@ -6,16 +6,13 @@ import io.github.mateuussilvapb.app_jm_perfumaria.application.produto.query.spec
 import io.github.mateuussilvapb.app_jm_perfumaria.domain.produto.Produto;
 import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.exceptions.ProdutoNotFoundException;
 import io.github.mateuussilvapb.app_jm_perfumaria.infra.produto.repository.IProdutoRepository;
-import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Situacao;
 import io.github.mateuussilvapb.app_jm_perfumaria.shared.enums.Status;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,43 +20,8 @@ public class ProdutoQueryService {
 
     private final IProdutoRepository produtoRepository;
 
-    public List<Produto> findAll(){
-        return this.produtoRepository.findAll();
-    }
-
-    public List<Produto> findAllAtivos() {
-        return this.produtoRepository.findAllByStatus(Status.ATIVO);
-    }
-
-    public List<Produto> findAllInativos() {
-        return this.produtoRepository.findAllByStatus(Status.INATIVO);
-    }
-
-    public List<Produto> findAllByTermAndStatus(String searchTerm, Status status) {
-        List<Produto> produtos;
-
-        if (status == Status.ATIVO) {
-            produtos = this.findAllAtivos();
-        } else {
-            produtos = this.findAllInativos();
-        }
-
-        if (StringUtils.isNotBlank(searchTerm)) {
-            produtos = produtos.stream()
-                    .filter(produto -> produto.matchSearchTerm(searchTerm))
-                    .collect(Collectors.toList());
-        }
-
-        return produtos;
-    }
-
-    public List<AutocompleteDTO> findAllToAutocompleteByTermAndStatus(String searchTerm,
-                                                                      Status status) {
+    public List<AutocompleteDTO> findAllToAutocompleteByTermAndStatus(String searchTerm, Status status) {
         return this.produtoRepository.findAllByStatusAndTermoAutocompleteDTO(searchTerm, status);
-    }
-
-    public List<Produto> findAllBySituacaoAndStatus(Situacao situacao, Status status) {
-        return this.produtoRepository.findAllBySituacaoAndStatus(situacao, status);
     }
 
     public Produto findById(Long id) {
@@ -67,7 +29,6 @@ public class ProdutoQueryService {
     }
 
     public Page<Produto> findByFilters(ProdutoFiltersDTO filtros, Pageable pageable) {
-        return produtoRepository
-                .findAll(ProdutoSpecification.filtrar(filtros), pageable);
+        return produtoRepository.findAll(ProdutoSpecification.filtrar(filtros), pageable);
     }
 }
