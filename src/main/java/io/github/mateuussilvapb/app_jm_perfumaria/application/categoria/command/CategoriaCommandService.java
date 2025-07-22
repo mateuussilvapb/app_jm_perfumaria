c087c73ba7e3f595fe6303b05c434f48428e4ab7
Mateus Dias <mateuussilvapb@gmail.com>
2025-07-22 07:36:08 -0300

    [feature] [sp02] [mssousa] Ajustes na exclusão de categoria

--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/categoria/command/CategoriaCommandService.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/application/categoria/command/CategoriaCommandService.java
@@ -20,9 +20,7 @@ public class CategoriaCommandService {
 
     public void deleteById(Long id) {
         var categoria = categoriaQueryService.findById(id);
-        categoria.setStatus(Status.INATIVO);
-        this.update(id, new CreateUpdateDto(categoria.getNome(), categoria.getDescricao(),
-                categoria.getStatus()));
+        this.categoriaRepository.deleteById(categoria.getId());
     }
 
     public Categoria update(Long id, CreateUpdateDto updatedCategoria) {
@@ -36,15 +34,13 @@ public class CategoriaCommandService {
     public Categoria create(CreateUpdateDto createdCategoria) {
         var categoriaSameName = categoriaRepository.findByNome(createdCategoria.nome());
         if (categoriaSameName.isPresent() && categoriaSameName.get().getStatus() == Status.INATIVO) {
-            var updatedCategoria = new CreateUpdateDto(createdCategoria.nome(),
-                    createdCategoria.descricao(), Status.ATIVO);
+            var updatedCategoria = new CreateUpdateDto(createdCategoria.nome(), createdCategoria.descricao(), Status.ATIVO);
             return this.update(categoriaSameName.get().getId(), updatedCategoria);
         }
         if (categoriaSameName.isPresent() && categoriaSameName.get().getStatus() == Status.ATIVO) {
             throw new CategoriaSameNameException(categoriaSameName.get().getNome());
         }
-        var categoria = new Categoria(createdCategoria.nome(), createdCategoria.descricao(),
-                Status.ATIVO, new ArrayList<>());
+        var categoria = new Categoria(createdCategoria.nome(), createdCategoria.descricao(), Status.ATIVO, new ArrayList<>());
         return categoriaRepository.save(categoria);
     }
 
