fdbd6be58641985e3f6dae0102e1cc8fa6caf21b
Mateus Dias <mateuussilvapb@gmail.com>
2025-08-22 19:51:19 -0300

    [feature] [sp02] [mssousa] Método de consulta no repository para buscar todos os ProdutoEntradaEstoque pelo id do EntradaEstoque.

--- src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/produtoEntradaEstoque/repository/IProdutoEntradaEstoqueRepository.java
+++ src/main/java/io/github/mateuussilvapb/app_jm_perfumaria/infra/produtoEntradaEstoque/repository/IProdutoEntradaEstoqueRepository.java
@@ -3,8 +3,20 @@ package io.github.mateuussilvapb.app_jm_perfumaria.infra.produtoEntradaEstoque.r
 import io.github.mateuussilvapb.app_jm_perfumaria.domain.produtoEntradaEstoque.ProdutoEntradaEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IProdutoEntradaEstoqueRepository extends JpaRepository<ProdutoEntradaEstoque, Long>, JpaSpecificationExecutor<ProdutoEntradaEstoque> {
+
+    @Query("""
+            SELECT p
+            FROM ProdutoEntradaEstoque p
+            JOIN FETCH p.produto
+            WHERE p.entradaEstoque.id = :entradaEstoqueId
+            """)
+    List<ProdutoEntradaEstoque> findAllByEntradaEstoqueId(Long entradaEstoqueId);
+
 }
